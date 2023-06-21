7f2a2485f65aad821a574612f8a79dd64ca33a0f
Aria Muhammad Luthfi <ariamuhammad77@gmail.com>
2023-06-21 13:55:14 +0700

    First

---
+++ Assets/Script/Tutup.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutup : MonoBehaviour
+{
+    public void TutupInfo()
+    {
+        Debug.Log("Tutup");
+        Destroy(gameObject);
+    }
+}
